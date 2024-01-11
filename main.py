f31392f7006bb410dae05407c484ee6996041367
Hemant <76446512+Hemantraj19@users.noreply.github.com>
2024-01-12 01:56:03 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,8 +2,8 @@ from InternetSpeedTwitterBot import InternetSpeedTwitterBot
 
 PROMISED_DOWN = 100
 PROMISED_UP = 100
-TWITTER_EMAIL = "captainmarvel.cm11@gmail.com"
-TWITTER_PASSWORD = "u3G%L4R)Y-f?S,3"
+TWITTER_EMAIL = ""
+TWITTER_PASSWORD = ""
 
 internet_speed = InternetSpeedTwitterBot()
 internet_speed.get_internet_speed()
